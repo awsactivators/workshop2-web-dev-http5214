77973165a19436f355d0230c722a02497cc3d775
Genevieve <awavieve@gmail.com>
2025-02-22 12:39:23 -0500

    Add firebase project files

---
+++ week7/firebase/README.md
@@ -0,0 +1,28 @@
+# Firebase Sandbox
+
+A sandbox for messing around with Firebase.
+
+Hosting URL: [Firebase](https://workshop-9f97f.web.app)
+
+---
+
+## Repo Resources
+
+- [Firebase](https://firebase.google.com/)
+
+<br>
+<a href="https://codeadam.ca">
+<img src="https://cdn.codeadam.ca/images@1.0.0/codeadam-logo-coloured-horizontal.png" width="200">
+</a>
+
+
+<!-- Firebase Deploy
+-- global installations --
+npm install -g firebase-tools
+firebase --version
+firebase login
+
+-- root project folder --
+firebase init hosting
+firebase deploy 
+-->
---
+++ week7/firebase/index.js
@@ -0,0 +1,88 @@
+// Documentation
+// https://firebase.google.com/docs/database/web/read-and-write
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  child,
+  get,
+  push,
+  set,
+  onValue,
+  serverTimestamp,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+// Fetch messages
+const messages = ref(database, "/messages");
+
+// On data event
+onValue(
+  messages,
+  (snapshot) => {
+    // Create a reference to the ul element
+    const ul = document.getElementById("messages");
+
+    // Empty the ul emelemt
+    ul.replaceChildren();
+
+    // Loop through messages
+    snapshot.forEach((childSnapshot) => {
+      // Get key and children
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+
+      console.log(childKey);
+      console.log(childData);
+
+      // Add message to list
+      const text = document.createTextNode(
+        childData.message + " ~ " + childData.name
+      );
+      const li = document.createElement("li");
+      li.appendChild(text);
+      ul.appendChild(li);
+    });
+  },
+  {
+    onlyOnce: false,
+  }
+);
+
+let add = document.getElementById("add");
+
+add.addEventListener("click", function (e) {
+  const name = document.getElementById("name");
+  const message = document.getElementById("message");
+
+  const newMessageRef = push(messages);
+
+  set(newMessageRef, {
+    name: name.value,
+    message: message.value,
+    createdAt: serverTimestamp(),
+  });
+
+  e.preventDefault();
+});
